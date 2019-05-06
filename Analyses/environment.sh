c5339c39d1bfcbca56b64dd673205f18a9006776
jdulemba <josdrw9@gmail.com>
2019-05-06 10:40:36 +0200

    Changed jobid

--- Analyses/environment.sh
+++ Analyses/environment.sh
@@ -6,5 +6,6 @@ branch=$( git symbolic-ref --short HEAD )
 echo "On branch '$branch' of $git_dir"
 
 
-export jobid=2018Nov08
+#export jobid=2018Nov08
+export jobid=leadpt50_MT40 ## same files as 2018Nov08 but different MT and leading jet pt cuts
 echo "jobid=$jobid"
