630af579b65ce528265fbb6e97f393bdeb1aa2d1
jdulemba <josdrw9@gmail.com>
2019-05-16 22:20:44 +0200

    Added jobids for 2017 and 2018 data+MC

--- Analyses/jobid.sh
+++ Analyses/jobid.sh
@@ -1,2 +1,10 @@
+	## 2016 jobids
+
 #export jobid=2018Nov08 #same files as 2017Aug24 but with no preselection
-export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
+#export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
+
+	## 2017 jobids
+export jobid=deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2017 ntuples using deepjet bdiscr
+
+	## 2018 jobids
+#export jobid=htt_2018data_deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2018 ntuples using deepjet bdiscr
