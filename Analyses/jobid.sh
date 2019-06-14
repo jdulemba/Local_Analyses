2633d55674a00ec2296ec9961671eb8fadd36c43
jdulemba <josdrw9@gmail.com>
2019-06-14 14:01:42 +0200

    Added jobid for 2016Legacy

--- Analyses/jobid.sh
+++ Analyses/jobid.sh
@@ -1,2 +1,7 @@
+    ## original 2016 files
 #export jobid=2018Nov08 #same files as 2017Aug24 but with no preselection
-export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
+#export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
+
+
+    ## 2016 legacy files
+export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
