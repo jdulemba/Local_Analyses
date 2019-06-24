0c05220ec62d20644f630e7c2c777e1e0b5fd5ec
jdulemba <josdrw9@gmail.com>
2019-06-24 11:51:57 +0200

    jobid for original 2016 datasets used

--- Analyses/jobid.sh
+++ Analyses/jobid.sh
@@ -1,10 +1,10 @@
 	## original 2016 jobids
 
 #export jobid=2018Nov08 #same files as 2017Aug24 but with no preselection
-#export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
+export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
 
     ## 2016 legacy files
-export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
+#export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
 
 	## 2017 jobids
 #export jobid=deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2017 ntuples using deepjet bdiscr
