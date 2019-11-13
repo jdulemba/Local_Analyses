e55b0aeab170d27921d3580897856acf05571dbb
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2019-11-13 19:43:51 +0100

    Added new 2018 jobid

--- Analyses/jobid.sh
+++ Analyses/jobid.sh
@@ -4,10 +4,11 @@
 #export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
 
     ## 2016 legacy files
-export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
+#export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
 
 	## 2017 jobids
 #export jobid=deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2017 ntuples using deepjet bdiscr
 
 	## 2018 jobids
 #export jobid=htt_2018data_deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2018 ntuples using deepjet bdiscr
+export jobid=htt_2018data_deepjet_j20_l50_MT40_JECv19_JERv7 # leading jet pt > 50 and MT > 40 with 2018 JECv19_JERv7 ntuples using deepjet bdiscr
