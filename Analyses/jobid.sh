8c12b58b64e4280a40161a9534da9998fec91dd1
Joseph Dulemba <jdulemba@Too-MacBook-Air.local>
2020-01-31 09:31:29 +0100

    Added new jobid to investigate issue with 2017 data+MC agreement

--- Analyses/jobid.sh
+++ Analyses/jobid.sh
@@ -4,11 +4,15 @@
 #export jobid=leadpt50_MT40 #same files as 2018Nov08 but leading jet pt > 50 and MT > 40
 
     ## 2016 legacy files
-#export jobid=DeepJet_2016Legacy_leadpt50_MT40 #leading jet pt > 50 and MT > 40
+#export jobid=htt_DeepJet_2016Legacy_j20l50MT40
 
 	## 2017 jobids
-#export jobid=deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2017 ntuples using deepjet bdiscr
+#export jobid=htt_2017data_DeepJet_j20l50MT40 # leading jet pt > 50 and MT > 40 with 2017 ntuples using deepjet bdiscr
 
 	## 2018 jobids
 #export jobid=htt_2018data_deepjet_leadpt50_MT40 # leading jet pt > 50 and MT > 40 with 2018 ntuples using deepjet bdiscr
-export jobid=htt_2018data_deepjet_j20_l50_MT40_JECv19_JERv7 # leading jet pt > 50 and MT > 40 with 2018 JECv19_JERv7 ntuples using deepjet bdiscr
+#export jobid=htt_2018data_deepjet_j20_l50_MT40_JECv19_JERv7 # leading jet pt > 50 and MT > 40 with 2018 JECv19_JERv7 ntuples using deepjet bdiscr
+#export jobid=htt_DeepJet_2018data_j30l50MT40 
+
+	## year independent
+export jobid=run2 # used for comparing 2016, 2017, and 2018 for debugging 2017
